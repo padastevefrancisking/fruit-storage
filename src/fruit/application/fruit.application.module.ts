3ece408836ac59032a3cd0f658b3793649d3b21d
Steve Francis King Pada <padastevefrancisking@gmail.com>
2026-09-18 03:56:03 +0800

    feat(fruit-infra): add module for application and infrastructure layer

---
+++ src/fruit/application/fruit.application.module.ts
@@ -0,0 +1,33 @@
+import { Module } from "@nestjs/common";
+import { CreateFruitUseCase } from "./use-cases/create-fruit.use-case";
+import { DeleteFruitUseCase } from "./use-cases/delete-fruit.use-case";
+import { FindFruitUseCase } from "./use-cases/find-fruit.use-case";
+import { ListFruitsUseCase } from "./use-cases/list-fruits.use-case";
+import { RemoveFruitUseCase } from "./use-cases/remove-fruit.use-case";
+import { StoreFruitUseCase } from "./use-cases/store-fruit.use-case";
+import { UpdateFruitUseCase } from "./use-cases/update-fruit.use-case";
+import { FruitInfrastructureModule } from "../infra/fruit.infrastructure.module";
+import { FruitUniquenessService } from "../domain/fruit-uniqueness";
+import { FruitFactory } from "../domain/factory/fruit.factory";
+
+const useCases = [
+    CreateFruitUseCase,
+    DeleteFruitUseCase,
+    FindFruitUseCase,
+    ListFruitsUseCase,
+    RemoveFruitUseCase,
+    StoreFruitUseCase,
+    UpdateFruitUseCase
+];
+
+@Module({
+    imports: [ FruitInfrastructureModule ],
+    controllers: [],
+    providers: [ 
+        FruitUniquenessService, 
+        FruitFactory,
+        ...useCases
+    ],
+    exports: [...useCases]
+})
+export class FruitApplicationModule {}
---
+++ src/fruit/infra/fruit.infrastructure.module.ts
@@ -0,0 +1,31 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ScheduleModule } from "@nestjs/schedule";
+import { FruitDocument, FruitSchema } from "./database/fruit.schema";
+import { OutboxEventDocument, OutboxEventSchema } from "./database/outbox/outbox.schema";
+import { OUTBOX_REPOSITORY } from "./database/outbox/repos/outbox.repository";
+import { OutboxRepository } from "./database/outbox/repos/implementations/outbox.repository.impl";
+import { FRUIT_REPOSITORY } from "../repos/fruit.repository";
+import { MongooseFruitRepository } from "./database/outbox/repos/implementations/mongoose-fruit.repository";
+import { EVENT_PUBLISHER } from "./events/event-publisher";
+import { MockEventPublisher } from "./events/implementations/mock-event-publisher";
+import { OutboxDispatcherCron } from "./events/outbox-dispatcher.cron";
+
+@Module({
+    imports: [
+        ScheduleModule.forRoot(),
+        MongooseModule.forFeature([
+            { name: FruitDocument.name, schema: FruitSchema },
+            { name: OutboxEventDocument.name, schema: OutboxEventSchema }
+        ])
+    ],
+    controllers: [],
+    providers: [
+        { provide: OUTBOX_REPOSITORY, useClass: OutboxRepository },
+        { provide: FRUIT_REPOSITORY, useClass: MongooseFruitRepository },
+        { provide: EVENT_PUBLISHER, useClass: MockEventPublisher },
+        OutboxDispatcherCron
+    ],
+    exports: [ FRUIT_REPOSITORY ]
+})
+export class FruitInfrastructureModule {}
