ce149ff3104242044f9972d3976ea01869a99f80
Steve Francis King Pada <padastevefrancisking@gmail.com>
2026-09-22 07:52:16 +0800

    feat(fruit-interfaces): define GraphQL schema, types, and context

---
+++ src/fruit/interfaces/graphql/schema.ts
@@ -0,0 +1,12 @@
+import { makeSchema } from "nexus";
+import { FruitMutation, FruitQuery, FruitType } from "./fruit.graphql-types";
+import { join } from "path";
+
+export const fruitSchema = makeSchema({
+    types: [FruitType, FruitQuery, FruitMutation],
+    outputs: {
+        schema: join(process.cwd(), 'generated', 'schema.graphql'),
+        typegen: join(process.cwd(), 'generated', 'nexus-typegenAutoConfig.ts')
+    },
+    shouldGenerateArtifacts: process.env.NODE_ENV !== 'production'
+})
